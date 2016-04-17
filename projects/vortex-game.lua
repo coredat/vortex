90f2022fd1137d51b18b49d996fb6fe16102d37e
PhilCK <phil@cooperking.net>
2016-04-17 20:09:47 +0100

    update lua.

--- projects/vortex-game.lua
+++ projects/vortex-game.lua
@@ -1,6 +1,6 @@
 
 
-function get_proj_root_dir()
+function get_solution_root_dir()
   if(os.get() == "macosx") then
     --return "$(SRCROOT)/../"
     return "../"
@@ -9,7 +9,9 @@ function get_proj_root_dir()
   end
 end
 
+
 solution "VortexApplication"
+  location("./")
 
    configurations {
      "Debug",
@@ -17,6 +19,7 @@ solution "VortexApplication"
    }
 
    project "VortexGameCode"
+      location("./")
 
       kind "WindowedApp"
       language "C++"
@@ -31,22 +34,46 @@ solution "VortexApplication"
         "../code/**.md",
       }
 
+      if(os.get() == "macosx") then
+        libdirs {
+          "/usr/local/lib/",
+          -- "../../core/3rdparty/SDL/lib/win32/",
+          -- "../../core/3rdparty/GLEW/lib/win32/",
+          -- "../../core/3rdparty/SOIL/lib/win32/",
+        }
+      else
       libdirs {
         "/usr/local/lib/",
+        "../../core/3rdparty/sdl/lib/win32/",
+        "../../core/3rdparty/glew/lib/win32/",
+        "../../core/3rdparty/soil/lib/win32/",
       }
+      end
 
+      if(os.get() == "macosx") then
       linkoptions {
         "-lsdl2",
         "-lsdl2_mixer",
         "-lSOIL",
       }
+      end
 
+      if(os.get() == "macosx") then
       links {
         "OpenGL.framework",
         "CoreFoundation.framework",
         "Foundation.framework",
         "CoreEngine",
       }
+      else
+      links {
+        "CoreEngine",
+        "glew32.lib",
+        "sdl2.lib",
+        "SOIL.lib",
+        "opengl32.lib",
+      }
+      end
 
       includedirs {
         "../../core/3rdparty/sdl_mixer/include/",
@@ -57,21 +84,23 @@ solution "VortexApplication"
         "../../core/code/external/math",
         "../../core/code/external/graphics_api",
         "../../core/code/external/utilities",
-        get_proj_root_dir() .. "code/",
+        get_solution_root_dir() .. "code/",
       }
 
+      if(os.get() == "macosx") then
       buildoptions {
         "-std=c++14",
         "-stdlib=libc++",
-      } --, "-framework OpenGL", "-framework CoreFoundation"}
+      }
+      end
 
       configuration "Debug"
          defines { "DEBUG" }
-         flags { "Symbols" }
+         flags { "Symbols", "Unicode"}
 
       configuration "Release"
          defines { "NDEBUG" }
-         flags { "Optimize" }
+         flags { "Optimize", "Unicode" }
 
 
    dofile("../../core/projects/core.lua")
