07e3a633937ffeb5cc2e3c07adb69b6c0a30a456
Phil Cooper-King <phil@cooperking.net>
2016-08-15 19:57:36 +0100

    Still working on unproject, still kinda off still.

--- code/game_states/title_screen.cpp
+++ code/game_states/title_screen.cpp
@@ -58,7 +58,7 @@ title_screen_init(Core::Context &ctx,
 
   if(!title_screen)
   {
-    Core::Model model(Core::Directory::volatile_resource_path("assets/models/unit_plane.obj"));
+    Core::Model model(Core::Directory::volatile_resource_path("assets/models/unit_cube.obj"));
     assert(model);
     
     Core::Material_renderer mat_renderer;
@@ -68,9 +68,10 @@ title_screen_init(Core::Context &ctx,
     title_screen = Core::Entity(world);
     
     Core::Rigidbody rb;
-    rb.set_collider(Core::Box_collider(1,1,1));
+//    rb.set_collider(Core::Box_collider(1.15, 0.5, 0.3));
+    rb.set_collider(Core::Box_collider(2,2,2));
     rb.set_is_dynamic(false);
-    
+    rb.set_is_trigger(true);
     title_screen.set_rigidbody(rb);
     
     title_screen.set_name("title_screen[title]");
@@ -78,8 +79,9 @@ title_screen_init(Core::Context &ctx,
     title_screen.set_renderer(mat_renderer);
     title_screen.set_transform(Core::Transform(
       math::vec3_zero(),
-      math::vec3_init(512.f, 1.f, 128.f),
-      math::quat_init_with_axis_angle(Core::Transform::get_world_left(), -math::quart_tau())
+      math::vec3_init(100, 100, 0.5),
+      math::quat()
+//      math::quat_init_with_axis_angle(Core::Transform::get_world_left(), 0.f)//-math::quart_tau())
     ));
     
 
@@ -134,41 +136,36 @@ title_screen_update(Core::Context &ctx,
   // Mouse Pick Test
   {
     // Norm coords
-    Core::Axis mouse = Core::Input::mouse_get_coordinates(ctx);
-//    mouse.x = (mouse.x / (ctx.get_width() * 0.5f)) - 1.f;
-//    mouse.y = (mouse.y / (ctx.get_height() * 0.5f)) - 1.f;
-    mouse.x = (2.f * mouse.x) / ctx.get_width() - 1.f;
-    mouse.y = 1.f - (2.f * mouse.y) / ctx.get_height();
+    Core::Axis mouse_coords = Core::Input::mouse_get_coordinates(ctx);
+    mouse_coords.x = (2.f * mouse_coords.x) / ctx.get_width() - 1.f;
+    mouse_coords.y = 1.f - (2.f * mouse_coords.y) / ctx.get_height();
     
-    
-    const math::mat4 world_mat = Core::Transform_utils::get_world_matrix(title_screen.get_transform());
-    const math::mat4 view_mat = Core::Camera_utils::camera_get_view_matrix(camera);
-    const math::mat4 proj_mat = Core::Camera_utils::camera_get_projection_matrix(camera);
-    const math::mat4 proj_view_mat = math::mat4_multiply(view_mat, proj_mat);
-    
-    const math::vec4 mouse_clip_pos = math::mat4_multiply(math::vec4_init(mouse.x, mouse.y, 0, 1), proj_view_mat);
-    const math::vec4 mouse_world_pos =
-    
-    
-    const math::mat4 wvp_mat = math::mat4_multiply(world_mat, view_mat, proj_mat);
-    const math::mat4 wvp_inv_mat = math::mat4_get_inverse(wvp_mat);
-    
-    const math::vec4 near = math::mat4_multiply(math::vec4_init(mouse.x, mouse.y, 0, 1.f), wvp_inv_mat);
-    const math::vec4 ray_start = math::vec4_init(math::mat4_get(wvp_inv_mat, 12),
-                                                 math::mat4_get(wvp_inv_mat, 13),
-                                                 math::mat4_get(wvp_inv_mat, 14),
-                                                 math::mat4_get(wvp_inv_mat, 15));
-    
-    const math::vec4 ray_dir_unnorm = math::vec4_subtract(near, math::vec4_normalize(ray_start));
-    const math::vec3 ray_dir = math::vec3_normalize(math::vec3_init(math::get_x(ray_dir_unnorm), math::get_y(ray_dir_unnorm), math::get_z(ray_dir_unnorm)));
+    const math::vec4 mouse_clip = math::vec4_init(mouse_coords.x, mouse_coords.y, 0.f, 1.f);
 
+//    mouse_worldspace = inverse(C) * inverse(P) * mouse_clip
+    const math::mat4 world_mat = Core::Transform_utils::get_world_matrix(camera.get_attached_entity().get_transform());
+    const math::mat4 world_inv_mat = math::mat4_get_inverse(world_mat);
     
-    Core::Ray ray(world, math::vec3_init(math::get_x(ray_start), math::get_y(ray_start), math::get_z(ray_start)), ray_dir);
+    const math::mat4 proj_mat = Core::Camera_utils::camera_get_projection_matrix(camera);
+    const math::mat4 proj_inv_mat = math::mat4_get_inverse(proj_mat);
+    
+    const math::mat4 world_proj_inv_mat = math::mat4_multiply(world_inv_mat, proj_inv_mat);
+    const math::vec4 mouse_worldspace = math::mat4_multiply(mouse_clip, world_proj_inv_mat);
+    
+//    const math::vec4 ray_dir = math::vec4_normalize(mouse_worldspace);
+//    const math::vec3 norm_dir = math::vec3_init(math::get_x(ray_dir), math::get_y(ray_dir), math::get_z(ray_dir));
+  
+    const math::vec3 cam_pos = camera.get_attached_entity().get_transform().get_position();
+    const math::vec3 mouse_worldspace_norm = math::vec3_normalize(math::vec3_init(math::get_x(mouse_worldspace), math::get_y(mouse_worldspace), math::get_z(mouse_worldspace)));
+  
+    const math::vec3 mouse_pos = math::vec3_normalize(math::vec3_subtract(mouse_worldspace_norm, cam_pos));
+
+    Core::Ray ray(world, cam_pos, mouse_worldspace_norm);
 
   
 //    printf("Cam pos %f, %f, %f \n", math::get_x(cam_pos), math::get_y(cam_pos), math::get_z(cam_pos));
-    printf("Ray ori %f, %f, %f \n", math::get_x(ray_start), math::get_y(ray_start), math::get_z(ray_start));
-    printf("Ray dir %f, %f, %f \n", math::get_x(ray_dir), math::get_y(ray_dir), math::get_z(ray_dir));
+//    printf("Ray ori %f, %f, %f \n", math::get_x(ray_start), math::get_y(ray_start), math::get_z(ray_start));
+//    printf("Ray dir %f, %f, %f \n", math::get_x(ray_dir), math::get_y(ray_dir), math::get_z(ray_dir));
     
     if(ray.has_hit())
     {
